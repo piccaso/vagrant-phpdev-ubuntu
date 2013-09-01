b57d2c6aa6fb58671ef225b3ddd0264bf8d630a4
mamor <mamor.dev@gmail.com>
2013-09-02 00:58:11 +0900

    tab to space * 2

--- cookbooks/phpdev/recipes/default.rb
+++ cookbooks/phpdev/recipes/default.rb
@@ -11,181 +11,181 @@
 # initialize
 #
 template '/home/vagrant/.bashrc' do
-	user 'vagrant'
-	group 'vagrant'
+  user 'vagrant'
+  group 'vagrant'
 end
 
 execute 'apt-get' do
-	command 'apt-get update'
+  command 'apt-get update'
 end
 
 #
 # install php and apache
 #
 apt_repository 'php5' do
-	uri 'http://ppa.launchpad.net/ondrej/php5/ubuntu'
-	distribution node['lsb']['codename']
-	components ['main']
-	keyserver 'keyserver.ubuntu.com'
-	key 'E5267A6C'
+  uri 'http://ppa.launchpad.net/ondrej/php5/ubuntu'
+  distribution node['lsb']['codename']
+  components ['main']
+  keyserver 'keyserver.ubuntu.com'
+  key 'E5267A6C'
 end
 
 apt_repository 'apache2' do
-	uri 'http://ppa.launchpad.net/ondrej/apache2/ubuntu'
-	distribution node['lsb']['codename']
-	components ['main']
-	keyserver 'keyserver.ubuntu.com'
-	key 'E5267A6C'
+  uri 'http://ppa.launchpad.net/ondrej/apache2/ubuntu'
+  distribution node['lsb']['codename']
+  components ['main']
+  keyserver 'keyserver.ubuntu.com'
+  key 'E5267A6C'
 end
 
 %w{php5 php5-dev php5-mysqlnd}.each do |p|
-	package p do
-		action :install
-	end
+  package p do
+    action :install
+  end
 end
 
 service 'apache2' do
-	supports :status => true, :restart => true, :reload => true
-	action [:enable, :start]
+  supports :status => true, :restart => true, :reload => true
+  action [:enable, :start]
 end
 
 execute 'a2enmod' do
-	command 'a2enmod rewrite' # apache will be restarted by template
+  command 'a2enmod rewrite' # apache will be restarted by template
 end
 
 #
 # install mysql
 #
 package 'mysql-server' do
-	action :install
-	notifies :run, 'execute[mysqladmin]'
-	notifies :run, 'execute[mysql]'
+  action :install
+  notifies :run, 'execute[mysqladmin]'
+  notifies :run, 'execute[mysql]'
 end
 
 service 'mysql' do
-	supports :status => true, :restart => true, :reload => true
-	action [:enable, :start]
+  supports :status => true, :restart => true, :reload => true
+  action [:enable, :start]
 end
 
 execute 'mysqladmin' do
-	action :nothing
-	command 'mysqladmin password -u root ' + node['mysql']['password']
+  action :nothing
+  command 'mysqladmin password -u root ' + node['mysql']['password']
 end
 
 execute 'mysql' do
-	action :nothing
-	command "mysql -u root -p#{node['mysql']['password']} -e \"GRANT ALL PRIVILEGES ON *.* TO root@'%' IDENTIFIED BY '#{node['mysql']['password']}' WITH GRANT OPTION\""
+  action :nothing
+  command "mysql -u root -p#{node['mysql']['password']} -e \"GRANT ALL PRIVILEGES ON *.* TO root@'%' IDENTIFIED BY '#{node['mysql']['password']}' WITH GRANT OPTION\""
 end
 
 #
 # install packages by apt-get
 #
 %w{git mongodb redis-server phpmyadmin}.each do |p|
-	package p do
-		action :install
-	end
+  package p do
+    action :install
+  end
 end
 
 service 'mongodb' do
-	supports :status => true, :restart => true, :reload => true
-	action [:enable, :start]
+  supports :status => true, :restart => true, :reload => true
+  action [:enable, :start]
 end
 
 service 'redis-server' do
-	supports :status => true, :restart => true, :reload => true
-	action [:enable, :start]
+  supports :status => true, :restart => true, :reload => true
+  action [:enable, :start]
 end
 
 link '/var/www/phpmyadmin' do
-	to '/usr/share/phpmyadmin'
+  to '/usr/share/phpmyadmin'
 end
 
 #
 # git settings
 #
 execute 'git-config-user-email' do
-	command "sudo -u vagrant -H git config --global user.email \"#{node['git']['user']['email']}\""
+  command "sudo -u vagrant -H git config --global user.email \"#{node['git']['user']['email']}\""
 end
 
 execute 'git-config-user-name' do
-	command "sudo -u vagrant -H git config --global user.name \"#{node['git']['user']['name']}\""
+  command "sudo -u vagrant -H git config --global user.name \"#{node['git']['user']['name']}\""
 end
  
 #
 # install packages by pecl
 #
 execute 'pecl-mongo' do
-	command 'pecl install mongo'
-	not_if {File.exists?('/usr/lib/php5/20121212/mongo.so')}
+  command 'pecl install mongo'
+  not_if {File.exists?('/usr/lib/php5/20121212/mongo.so')}
 end
 
 #
 # install packages by npm
 #
 apt_repository 'nodejs' do
-	uri 'http://ppa.launchpad.net/chris-lea/node.js/ubuntu'
-	distribution node['lsb']['codename']
-	components ['main']
-	keyserver 'keyserver.ubuntu.com'
-	key 'C7917B12'
+  uri 'http://ppa.launchpad.net/chris-lea/node.js/ubuntu'
+  distribution node['lsb']['codename']
+  components ['main']
+  keyserver 'keyserver.ubuntu.com'
+  key 'C7917B12'
 end
 
 package 'nodejs' do
-	action :install
+  action :install
 end
 
 %w{coffee-script jshint grunt-cli}.each do |p|
-	execute p do
-		command 'npm install -g ' + p
-	end
+  execute p do
+    command 'npm install -g ' + p
+  end
 end
 
 #
 # install packages by gem
 #
 %w{fluentd jsduck serverspec compass}.each do |p|
-	gem_package p do
-		action :install
-	end
+  gem_package p do
+    action :install
+  end
 end
 
 #
 # install PhantomJS
 #
 execute 'phantomjs' do
-	command <<-CMD
+  command <<-CMD
     wget https://phantomjs.googlecode.com/files/phantomjs-1.9.1-linux-x86_64.tar.bz2
     tar jxvf phantomjs-1.9.1-linux-x86_64.tar.bz2
     cp phantomjs-1.9.1-linux-x86_64/bin/phantomjs /usr/local/bin/phantomjs
     rm -r phantomjs-1.9.1-linux-x86_64
     rm phantomjs-1.9.1-linux-x86_64.tar.bz2
-	CMD
-	not_if {File.exists?('/usr/local/bin/phantomjs')}
+  CMD
+  not_if {File.exists?('/usr/local/bin/phantomjs')}
 end
 
 #
 # templates
 #
 template '/etc/php5/apache2/php.ini' do
-	notifies :restart, 'service[apache2]'
+  notifies :restart, 'service[apache2]'
 end
 
 template '/etc/php5/cli/php.ini' do
 end
 
 template '/etc/apache2/apache2.conf' do
-	notifies :restart, 'service[apache2]'
+  notifies :restart, 'service[apache2]'
 end
 
 template '/etc/mysql/my.cnf' do
-	notifies :restart, 'service[mysql]'
+  notifies :restart, 'service[mysql]'
 end
 
 #
 # run custom recipe
 #
 begin
-	include_recipe 'phpdev::custom'
+  include_recipe 'phpdev::custom'
 rescue Exception => error
-	# avoid Chef::Exceptions::RecipeNotFound
+  # avoid Chef::Exceptions::RecipeNotFound
 end
--- cookbooks/phpdev/recipes/tools.rb
+++ cookbooks/phpdev/recipes/tools.rb
@@ -11,101 +11,101 @@
 # install phpdev-tools
 #
 git '/home/vagrant/phpdev-tools' do
-	action :checkout
-	user 'vagrant'
-	group 'vagrant'
-	repository 'https://github.com/mp-php/phpdev-tools.git'
-	reference 'master'
+  action :checkout
+  user 'vagrant'
+  group 'vagrant'
+  repository 'https://github.com/mp-php/phpdev-tools.git'
+  reference 'master'
 end
 
 composer_project '/home/vagrant/phpdev-tools' do
-	action :install
+  action :install
 end
 
 link '/usr/local/bin/phpunit' do
-	to '/home/vagrant/phpdev-tools/vendor/bin/phpunit'
+  to '/home/vagrant/phpdev-tools/vendor/bin/phpunit'
 end
 
 link '/usr/local/bin/apigen' do
-	to '/home/vagrant/phpdev-tools/vendor/bin/apigen.php'
+  to '/home/vagrant/phpdev-tools/vendor/bin/apigen.php'
 end
 
 link '/usr/local/bin/php-cs-fixer' do
-	to '/home/vagrant/phpdev-tools/vendor/bin/php-cs-fixer'
+  to '/home/vagrant/phpdev-tools/vendor/bin/php-cs-fixer'
 end
 
 #
 # install fuel-dbdocs
 #
 git '/home/vagrant/fuel-dbdocs' do
-	action :checkout
-	user 'vagrant'
-	group 'vagrant'
-	repository 'https://github.com/mp-php/fuel-dbdocs.git'
-	reference 'master'
-	enable_submodules true
+  action :checkout
+  user 'vagrant'
+  group 'vagrant'
+  repository 'https://github.com/mp-php/fuel-dbdocs.git'
+  reference 'master'
+  enable_submodules true
 end
 
 composer_project '/home/vagrant/fuel-dbdocs' do
-	action :install
+  action :install
 end
 
 composer_project '/home/vagrant/fuel-dbdocs/fuel/packages/dbdocs' do
-	action :install
+  action :install
 end
 
 template '/home/vagrant/fuel-dbdocs/fuel/app/config/crypt.php' do
-	user 'vagrant'
-	group 'vagrant'
+  user 'vagrant'
+  group 'vagrant'
 end
 
 directory '/home/vagrant/fuel-dbdocs/fuel/app/logs' do
-	mode 0777
+  mode 0777
 end
 
 directory '/home/vagrant/fuel-dbdocs/public/dbdocs' do
-	mode 0777
+  mode 0777
 end
 
 link '/var/www/dbdocs' do
-	to '/home/vagrant/fuel-dbdocs/public'
+  to '/home/vagrant/fuel-dbdocs/public'
 end
 
 #
 # install RockMongo
 #
 git '/home/vagrant/rockmongo' do
-	action :checkout
-	user 'vagrant'
-	group 'vagrant'
-	repository 'https://github.com/iwind/rockmongo.git'
-	reference 'master'
+  action :checkout
+  user 'vagrant'
+  group 'vagrant'
+  repository 'https://github.com/iwind/rockmongo.git'
+  reference 'master'
 end
 
 link '/var/www/rockmongo' do
-	to '/home/vagrant/rockmongo'
+  to '/home/vagrant/rockmongo'
 end
 
 #
 # install phpRedisAdmin
 #
 git '/home/vagrant/phpredisadmin' do
-	action :checkout
-	user 'vagrant'
-	group 'vagrant'
-	repository 'https://github.com/ErikDubbelboer/phpRedisAdmin.git'
-	reference 'master'
+  action :checkout
+  user 'vagrant'
+  group 'vagrant'
+  repository 'https://github.com/ErikDubbelboer/phpRedisAdmin.git'
+  reference 'master'
 end
 
 execute 'rm composer.lock' do
-	command 'rm /home/vagrant/phpredisadmin/composer.lock'
-	not_if {File.exists?('/home/vagrant/phpredisadmin/vendor')}
+  command 'rm /home/vagrant/phpredisadmin/composer.lock'
+  not_if {File.exists?('/home/vagrant/phpredisadmin/vendor')}
 end
 
 composer_project '/home/vagrant/phpredisadmin' do
-	action :install
+  action :install
 end
 
 link '/var/www/phpredisadmin' do
-	to '/home/vagrant/phpredisadmin'
+  to '/home/vagrant/phpredisadmin'
 end
